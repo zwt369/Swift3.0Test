0a5e9e8826d4dc08e0005002c55b77067c7ba8de
装002 百变家 <baibianjiazhuang@baibianjiazhuang002demac-mini.local>
2016-10-07 10:54:03 +0800

    Initial Commit

---
+++ Swift3.0Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift3.0Test
+//
+//  Created by 百变家装002 on 16/10/7.
+//  Copyright © 2016年 百变家装002. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
